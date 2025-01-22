be278cc0a06a53d63322c34e14a74359ccaace49
Mohsen <90086302+MKS313@users.noreply.github.com>
2025-01-22 20:34:51 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import multiprocessing
 
 
-num_processes = 20  # 10
-num_workers = 20  # 20
+num_processes = 8  # 10
+num_workers = 8  # 20
 
 # num_workers = multiprocessing.Pool()._processes
 # num_cpu = multiprocessing.cpu_count()
 # print(num_workers)
 
-num_order = 1000
+num_order = 100
 
 
